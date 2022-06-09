9c649716611231c0114f9cccf63122ea1eb59dc3
Musab Shaikh <musabs2802@gmail.com>
2022-06-09 10:06:44 +0530

    feature ant design and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import { Navbar } from './components'
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
 
+import {
+  Exchanges,
+  HomePage,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+  Navbar,
+} from './components'
 import './App.css'
+
 const App = () => {
   return (
     <div className='app'>
       <div className='navbar'>
         <Navbar />
       </div>
-      <div className='main'></div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path='/' element={<HomePage />} />
+              <Route exact path='/exchanges' element={<Exchanges />} />
+              <Route
+                exact
+                path='/cryptocurrencies'
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route exact path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
       <div className='footer'></div>
     </div>
   )
